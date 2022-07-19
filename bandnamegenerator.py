aca0ac20c0d2841e612d7ad0880dbbd7a6c33900
ananyaanant1902 <99985776+ananyaanant1902@users.noreply.github.com>
2022-07-19 11:43:27 +0530

    Create bandnamegenerator.py

---
+++ bandnamegenerator.py
@@ -0,0 +1,8 @@
+name=input("What is your city of birth?")
+pet=input("What is your pet name?")
+print("Your band name is : "+name+" "+pet)
+
+#or you can do👇
+'''
+print(input("Your band name is "+input("What is your city of birth")+" "+input("What is your pet name?"))
+'''
